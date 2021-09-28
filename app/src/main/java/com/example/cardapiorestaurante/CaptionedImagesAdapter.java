7e2551576c0ead1adc1a7aa32bb7ad0e675a58d7
Augusto Paulo <augustopaulo123@hotmail.com>
2021-09-28 11:17:35 -0300

    Pasta.java + PizzaDetailActivity + pizza clickListener implemented

--- app/src/main/java/com/example/cardapiorestaurante/CaptionedImagesAdapter.java
+++ app/src/main/java/com/example/cardapiorestaurante/CaptionedImagesAdapter.java
@@ -3,6 +3,7 @@ package com.example.cardapiorestaurante;
 import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -12,11 +13,14 @@ import androidx.cardview.widget.CardView;
 import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 class CaptionedImagesAdapter extends
         RecyclerView.Adapter<CaptionedImagesAdapter.ViewHolder> {
 
     private String[] captions;
     private int[] imageIds;
+    private Listener listener;
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
 
@@ -28,6 +32,10 @@ class CaptionedImagesAdapter extends
         }
     }
 
+    interface Listener {
+        void onClick(int position);
+    }
+
     public CaptionedImagesAdapter(String[] captions, int[] imageIds) {
         this.captions = captions;
         this.imageIds = imageIds;
@@ -36,6 +44,10 @@ class CaptionedImagesAdapter extends
     @Override
     public int getItemCount() {
         return captions.length;
+    }
+
+    public void setListener(Listener listener) {
+        this.listener = listener;
     }
 
     @NonNull
@@ -56,5 +68,13 @@ class CaptionedImagesAdapter extends
         imageView.setContentDescription(captions[position]);
         TextView textView = (TextView) cardView.findViewById(R.id.info_text);
         textView.setText(captions[position]);
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (listener != null) {
+                    listener.onClick(holder.getAdapterPosition());
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/cardapiorestaurante/PastaFragment.java
+++ app/src/main/java/com/example/cardapiorestaurante/PastaFragment.java
@@ -4,30 +4,41 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.ListFragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
-public class PastaFragment extends ListFragment {
+public class PastaFragment extends Fragment {
 
     public PastaFragment() {
         // Required empty public constructor
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(
-                inflater.getContext(), android.R.layout.simple_list_item_1,
-                getResources().getStringArray(R.array.pasta));
-        setListAdapter(adapter);
-        return super.onCreateView(inflater, container, savedInstanceState);
+        RecyclerView pastaRecycler = (RecyclerView) inflater.inflate(
+                R.layout.fragment_pasta, container, false);
+
+        String[] pastaNames = new String[Pasta.pastas.length];
+        for (int i=0; i<pastaNames.length; i++) {
+            pastaNames[i] = Pasta.pastas[i].getName();
+        }
+
+        int[] pastaImages = new int[Pasta.pastas.length];
+        for(int i=0; i<pastaImages.length; i++) {
+            pastaImages[i] = Pasta.pastas[i].getImageResourceId();
+        }
+
+        CaptionedImagesAdapter adapter = new CaptionedImagesAdapter(pastaNames, pastaImages);
+        pastaRecycler.setAdapter(adapter);
+
+        GridLayoutManager layoutManager = new GridLayoutManager(getActivity(), 2);
+        pastaRecycler.setLayoutManager(layoutManager);
+        return pastaRecycler;
     }
 }
